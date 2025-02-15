3e43b891ee53c55f1269131c49f356c32c726311
Arto Bendiken <arto@asimov.so>
2025-02-15 06:50:59 +0100

    Enhance `Construct.open`.

--- asimov-construct/lib/asimov/construct.rb
+++ asimov-construct/lib/asimov/construct.rb
@@ -98,7 +98,8 @@ class ASIMOV::Construct
   # @yieldreturn [Object]
   # @return [ASIMOV::Construct] or `nil` if the construct does not exist
   def self.open(id, &block)
-    result = self.parse(ASIMOV::Config.constructs_dir.join(id.to_s))
+    id = id.is_a?(self.class) ? id.id : id.to_s
+    result = self.parse(ASIMOV::Config.constructs_dir.join(id))
     result = block.call(result) if block_given?
     result
   rescue Errno::ENOENT => error
